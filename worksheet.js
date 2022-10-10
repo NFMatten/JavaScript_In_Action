41d95553b2883e8c3c7ffce463655fd9898c58a8
Nikolaus Matten <nfmatten@gmail.com>
2022-10-10 08:51:33 -0600

    initial commit

---
+++ worksheet.js
@@ -0,0 +1,10 @@
+// Variables
+
+// Task 1: Days of the week
+let dayOfWeek = 'Monday';
+console.log(dayOfWeek);
+dayOfWeek = 'Friday';
+console.log(`I can't wait for ${dayOfWeek}!`);
+
+// Task 2: User input
+let animalInput = prompt("What's your favorite animal? ");
